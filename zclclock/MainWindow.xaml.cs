f9bf1cbb41b343c6b5cf2fde1d3c870378b04d8b
home-pc <2388674408@qq.com>
2020-08-09 21:33:27 +0800

    添加项目文件。

---
+++ zclclock/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+
+namespace zclclock
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            showtimer = new DispatcherTimer();
+            showtimer.Tick += new EventHandler(ShowCurTimer);
+            showtimer.Interval = new TimeSpan(0, 0, 0, 1, 0);
+            showtimer.Start();
+        }
+        bool B_alarm = false;
+        string alarmTime;
+        
+        public string nowtime=new TimeSpan(0,0,0).ToString();
+        private DispatcherTimer showtimer;
+        public void ShowCurTimer(object sender,EventArgs e)
+        {
+            timetxt.Text = DateTime.Now.ToString("HH:mm:ss");
+            
+            if (B_alarm)
+            {
+                if (alarmTime == timetxt.Text)
+                {
+                    //MessageBox.Show("time ");
+                    Window alarmwindow = new Window();
+                    alarmwindow.Height = 200;
+                    alarmwindow.Width = 200;
+                    alarmwindow.ShowInTaskbar = false;
+                    Button isok = new Button();
+                    isok.Content = "闹钟到了";
+                    isok.Margin = new Thickness(15);
+                    alarmwindow.Content = isok;
+                    alarmwindow.Show();
+
+                    //System.Media.SystemSounds.Asterisk.Play();
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            alarmTime = new TimeSpan(Convert.ToInt32(alarmH.Text), Convert.ToInt32(alarmM.Text), 0).ToString();
+            B_alarm = true;
+            L1.Content = "闹钟已设置为："+alarmTime;
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            B_alarm = false;
+            alarmH.Text = "12";
+            alarmM.Text = "0";
+            L1.Content = "";
+        }
+    }
+}
